8880489a56ac711442a2c6a6c2ca8bc74fbda7ae
LicenserX <licenset@gmail.com>
2018-11-11 14:29:04 +0300

    task ready

--- Lecture_7_Executors_Client_Server_SpringBoot_Json_RestTemplate/src/test/java/com/epam/executors/ScheduledThreadPool.java
+++ Lecture_7_Executors_Client_Server_SpringBoot_Json_RestTemplate/src/test/java/com/epam/executors/ScheduledThreadPool.java
@@ -1,14 +1,12 @@
 package com.epam.executors;
 
 import com.epam.threads.util.Util;
-import org.junit.Test;
-
-import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
+import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
@@ -22,6 +20,7 @@ public class ScheduledThreadPool {
         ScheduledFuture<String> result = executor.schedule(ScheduledThreadPool::healthCheck, 500, TimeUnit.MILLISECONDS);
 
         Thread.sleep(100);
+        result.cancel(true);
 
         assertEquals(true, result.isCancelled());
         executor.shutdown();
@@ -33,7 +32,7 @@ public class ScheduledThreadPool {
 
         ScheduledFuture<String> result = executor.schedule(ScheduledThreadPool::healthCheck, 500, TimeUnit.MILLISECONDS);
 
-        Thread.sleep(100);
+        Thread.sleep(2000);
         result.cancel(false);
 
         assertEquals("Result", result.get());

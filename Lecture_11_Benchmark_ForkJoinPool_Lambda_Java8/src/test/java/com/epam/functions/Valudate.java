72ff2afeaf6afc814930b1fdbdb8c1c5c3922fad
Roman_Samortoshenkov <Roman_Samotoshenkov@epam.com>
2018-11-18 14:19:47 +0300

    DZ Lect 11

--- Lecture_11_Benchmark_ForkJoinPool_Lambda_Java8/src/test/java/com/epam/functions/Valudate.java
+++ Lecture_11_Benchmark_ForkJoinPool_Lambda_Java8/src/test/java/com/epam/functions/Valudate.java
@@ -23,17 +23,17 @@ public class Valudate {
         BiFunction<Integer, Integer, Two> constructor = Two::new;
 
         Function<Integer, Two> constructorOneParam = Two::new;
-        Two two = constructorOneParam.apply(20); // TODO: fix in this line
+        Two two = constructorOneParam.apply(22); // TODO: fix in this line
         assertEquals((long) two.i, (long) 22);
 
-        Two two_ = constructor.apply(10, 10); // TODO: fix in this line
+        Two two_ = constructor.apply(11, 10); // TODO: fix in this line
         assertEquals((long) two_.i, (long) 11);
     }
 
     @Test
     public void testFlatMap() {
-        List<String> first = Arrays.asList("1", "2"); // TODO: fix in this line
-        List<String> second = Arrays.asList("3", "4"); // TODO: fix in this line
+        List<String> first = Arrays.asList("0", "3"); // TODO: fix in this line
+        List<String> second = Arrays.asList("3", "5"); // TODO: fix in this line
 
         List<List<String>> combined = Arrays.asList(first, second);
 

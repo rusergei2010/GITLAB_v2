8067fee23a1e25995a9f973a41e57d982ad6484b
Daniil9596 <daniil.shakurov95@gmail.com>
2018-11-21 15:29:32 +0300

    Lecture 6 is done

--- Lecture_6_part_1_ThreadExecutors_Working_Thread_Queue_Tasks_Singleton/src/test/java/com/ReentrantLockTest.java
+++ Lecture_6_part_1_ThreadExecutors_Working_Thread_Queue_Tasks_Singleton/src/test/java/com/ReentrantLockTest.java
@@ -19,7 +19,13 @@ public class ReentrantLockTest {
             lock.lock();
             Util.sleep(100);
             count++;
-            validate();
+            try {
+                validate();
+            } catch (RuntimeException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
         }
 
         private void validate() {

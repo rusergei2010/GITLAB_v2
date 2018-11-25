b922374ffd425103f0b6bf922df5ad604a29e2c9
Daniil9596 <daniil.shakurov95@gmail.com>
2018-11-25 16:19:51 +0300

    Lecture 14 is done

--- Lecture_14_Completable_Future_Concurrency_practice/src/test/java/home/Test2.java
+++ Lecture_14_Completable_Future_Concurrency_practice/src/test/java/home/Test2.java
@@ -17,7 +17,7 @@ public class Test2 {
     public void testSupply() throws ExecutionException, InterruptedException {
         CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(() -> "First Task");
         // TODO: Use com.data.lambda: (s -> s + " Second Task") with thenApply() termination operation for the CompletableFuture
-        CompletableFuture<String> result = completableFuture.supplyAsync(() -> " Second Task"); //thenApply(s -> s + " / Second Task");
+        CompletableFuture<String> result = completableFuture.thenApply(s -> s + " / Second Task");
 
         assertEquals("First Task / Second Task", result.get());
     }
@@ -31,6 +31,7 @@ public class Test2 {
         });
 
         // TODO: Put the blocking operation here to wait for the CompletableFuture's result being received
+        result.get();
         assertEquals(reference.get(), "Hey!");
     }
 
@@ -40,7 +41,7 @@ public class Test2 {
 
         CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(() -> "First Task. Hello");
 
-        BiFunction<String, String, String> func = (s1, s2) -> {return "";}; // TODO: Fix the return value in BiFunction
+        BiFunction<String, String, String> func = (s1, s2) -> {return s1 + s2;}; // TODO: Fix the return value in BiFunction
 
         CompletableFuture<String> combined = completableFuture.thenCombineAsync(CompletableFuture.supplyAsync(() -> " World"), func);
 
@@ -52,7 +53,7 @@ public class Test2 {
     public void testComposeAsyncSleep() throws ExecutionException, InterruptedException {
 
         CompletableFuture<String> completableFuture = CompletableFuture.supplyAsync(() -> {
-            sleep(500); // TODO: Fix the timeout here
+            sleep(1500); // TODO: Fix the timeout here
             return "First Task. Hello";
         });
         Thread.sleep(1000);
@@ -67,10 +68,12 @@ public class Test2 {
     public void testOfAnyOf() throws ExecutionException, InterruptedException {
         CompletableFuture<String> completableFuture1 = CompletableFuture.supplyAsync(() -> {
             sleep(ThreadLocalRandom.current().nextInt(500)); // line 1
+            sleep(500);
             return "One";
         });
         CompletableFuture<String> completableFuture2 = CompletableFuture.supplyAsync(() -> {
             sleep(ThreadLocalRandom.current().nextInt(2000)); // line 3
+            sleep(500);
             return "Two";
         });
         CompletableFuture<String> completableFuture3 = CompletableFuture.supplyAsync(() -> {

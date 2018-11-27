02c740b1b12d51176070c0706a469f419e4f9400
Ilya <ilia_iarlychev@epam.com>
2018-11-27 17:28:04 +0300

    lec9

--- Lecture_9_Threads_CountDownLatch_ReentrantLock_Condition/src/test/java/com/epam/test/SemaphoreTest.java
+++ Lecture_9_Threads_CountDownLatch_ReentrantLock_Condition/src/test/java/com/epam/test/SemaphoreTest.java
@@ -1,24 +1,24 @@
-package com.epam.test;
+package com.epam.test;// Benchmark Шипилев: @link{https://www.youtube.com/watch?v=8pMfUopQ9Es}
+
+
+import static org.junit.Assert.assertEquals;
 
 import com.epam.test.util.Util;
-
-import org.junit.Test;
-
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
+import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-
-
+// TODO: Fix one of the timeouts in Anchor 1 or Anchor 2
 public class SemaphoreTest {
 
     private static class Resource {
 
         private Semaphore semaphore;
-        int connections = 0;
+        AtomicInteger connections = new AtomicInteger(0);
 
         public Resource(Semaphore semaphore) {
             this.semaphore = semaphore;
@@ -27,23 +27,24 @@ public class SemaphoreTest {
         public void connect(String uri) {
             boolean permit = false;
             try {
-                permit = semaphore.tryAcquire(100, TimeUnit.MILLISECONDS); // Anchor 1
+                permit = semaphore.tryAcquire(500, TimeUnit.MILLISECONDS); // Anchor 1
                 if (permit) {
                     System.out.println("Connection established to " + uri);
-                    Util.threadSleep(5); // Anchor 2
-
-                    connections++;
+                    Util.threadSleep(100); // Anchor 2
+                    connections.getAndIncrement();
                 } else {
                     System.out.println("Connection rejected");
                 }
             } catch (InterruptedException e) {
                 e.printStackTrace();
             } finally {
-                if (permit)
+                if (permit) {
                     semaphore.release();
+                }
             }
         }
     }
+
     @Test
     public void testSemaphore() throws InterruptedException {
 
@@ -52,15 +53,12 @@ public class SemaphoreTest {
         Semaphore semaphore = new Semaphore(5);
         SemaphoreTest.Resource resource = new SemaphoreTest.Resource(semaphore);
 
-        IntStream.range(0, 10).forEach((port) -> {
-                    service.submit(() -> {
-                        resource.connect("localhost, " + (100 + port));
-                    });
-                }
+        IntStream.range(0, 10).forEach(
+                (port) -> service.submit(() -> resource.connect("localhost, " + (100 + port)))
         );
 
         Thread.sleep(4000);
-        assertEquals(10, resource.connections);
+        assertEquals(10, resource.connections.get());
 
         putDown(service, 4);
     }

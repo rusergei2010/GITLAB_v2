40a5c06bbdbdd513ab6cbc7f8f8877bb4814816e
Igor Volgin <igor_volgin@epam.com>
2018-11-27 09:11:50 +0300

    Lectures 4,5,6,7 done

--- Lecture_7_Executors_Client_Server_SpringBoot_Json_RestTemplate/src/test/java/com/epam/executors/CachedThreadPoolTest.java
+++ Lecture_7_Executors_Client_Server_SpringBoot_Json_RestTemplate/src/test/java/com/epam/executors/CachedThreadPoolTest.java
@@ -3,12 +3,7 @@ package com.epam.executors;
 import com.epam.threads.util.Util;
 import org.junit.Test;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
+import java.util.concurrent.*;
 
 import static com.epam.executors.TestUtil.shutdownWithDelay;
 import static junit.framework.TestCase.assertNotNull;
@@ -43,9 +38,9 @@ public class CachedThreadPoolTest {
         Future<String> result3 = service.submit(callable("Thread 3"));
 
 
-        assertEquals("Result Thread 1", result1);
-        assertEquals("Result Thread 2", result2);
-        assertEquals("Result Thread 3", result3);
+        assertEquals("Result Thread 1", result1.get());
+        assertEquals("Result Thread 2", result2.get());
+        assertEquals("Result Thread 3", result3.get());
 
         System.out.println("Exiting...");
 
@@ -59,8 +54,8 @@ public class CachedThreadPoolTest {
         ExecutorService service = Executors.newCachedThreadPool();
 
         Future<?> result1 = service.submit(runnable("Thread 1"));
-        service.shutdown();
         Future<?> result2 = service.submit(runnable("Thread 2"));
+        service.shutdown();
 
         assertNotNull(result1);
         assertNull(result1.get());

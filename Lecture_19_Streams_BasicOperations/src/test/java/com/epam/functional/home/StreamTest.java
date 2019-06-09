8c5c482bcab936b587205834b9877e88bb7b9501
S.Zheleznov <S.Zheleznov@thomsonreuters.com>
2019-06-09 18:11:27 +0300

    Added some unit tests

--- Lecture_19_Streams_BasicOperations/src/test/java/com/epam/functional/home/StreamTest.java
+++ Lecture_19_Streams_BasicOperations/src/test/java/com/epam/functional/home/StreamTest.java
@@ -1,13 +1,9 @@
 package com.epam.functional.home;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -19,3 +19,7 @@ cd $TRAVIS_BUILD_DIR/Lecture_11_Barrier_CAS
 mvn test
 cd $TRAVIS_BUILD_DIR/Lecture_14_Completable_Future_Concurrency_practice
 mvn test
+cd $TRAVIS_BUILD_DIR/Lecture_15_Concurrent_Collection
+mvn test
+cd $TRAVIS_BUILD_DIR/Lecture_19_Streams_BasicOperations
+mvn test

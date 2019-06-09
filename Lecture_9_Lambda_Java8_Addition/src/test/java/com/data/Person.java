99ec03d1d045fc20ab5032f45aaadb80b7ee5cd2
S.Zheleznov <S.Zheleznov@thomsonreuters.com>
2019-06-09 16:28:32 +0300

    Fix builds

--- Lecture_9_Lambda_Java8_Addition/src/test/java/com/data/Person.java
+++ Lecture_9_Lambda_Java8_Addition/src/test/java/com/data/Person.java
@@ -1,8 +1,6 @@
 package com.data;
 
-import org.apache.commons.lang3.builder.EqualsBuilder;
-import org.apache.commons.lang3.builder.HashCodeBuilder;
-import org.apache.commons.lang3.builder.ToStringBuilder;
+import java.util.Objects;
 
 public class Person {
     private final String firstName;
@@ -47,36 +45,28 @@ public class Person {
         System.out.println(this.toString());
     }
 
-    @Override
-    public String toString() {
-        return new ToStringBuilder(this)
-                .append("firstName", firstName)
-                .append("lastName", lastName)
-                .append("age", age)
-                .toString();
+    @Override public String toString() {
+        return "Person{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Person person = (Person) o;
-
-        return new EqualsBuilder()
-                .append(age, person.age)
-                .append(firstName, person.firstName)
-                .append(lastName, person.lastName)
-                .isEquals();
+    @Override public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final Person person = (Person) o;
+        return age == person.age &&
+                Objects.equals(firstName, person.firstName) &&
+                Objects.equals(lastName, person.lastName);
     }
 
-    @Override
-    public int hashCode() {
-        return new HashCodeBuilder(17, 37)
-                .append(firstName)
-                .append(lastName)
-                .append(age)
-                .toHashCode();
+    @Override public int hashCode() {
+        return Objects.hash(firstName, lastName, age);
     }
 }

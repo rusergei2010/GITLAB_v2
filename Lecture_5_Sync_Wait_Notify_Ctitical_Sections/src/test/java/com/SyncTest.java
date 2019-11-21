462055a7911fce03d1a1da5cc971ec7945e91f24
Anton Ishchenko <anton_ishchenko@epam.com>
2019-11-21 11:59:28 +0300

    Lecture5 all tests passed

--- Lecture_5_Sync_Wait_Notify_Ctitical_Sections/src/test/java/com/SyncTest.java
+++ Lecture_5_Sync_Wait_Notify_Ctitical_Sections/src/test/java/com/SyncTest.java
@@ -17,17 +17,17 @@ public class SyncTest {
     Lock lock = new ReentrantLock();
 
     public void change() {
-
-        lock.lock();
-        try {
+        if(lock.tryLock()) {
             try {
-                Thread.sleep(1000);
-            } catch (Exception e){
-                e.printStackTrace();
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                counter++;
+            } finally {
+                lock.unlock();
             }
-            counter++;
-        } finally {
-            lock.unlock();
         }
     }
 

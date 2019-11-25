b68b49bba8ef6b75ce6734717525274b68aa73c5
Vadim Salimov <vadim.mcmxc@gmail.com>
2019-11-25 17:40:45 +0300

    first step

--- Lecture_4_Threads_Intro/src/test/java/com/AppTestThreadState.java
+++ Lecture_4_Threads_Intro/src/test/java/com/AppTestThreadState.java
@@ -11,6 +11,7 @@ public class AppTestThreadState {
 
     /**
      * Fill in the gaps and insert instructions to make code executable
+     *
      * @throws InterruptedException
      */
     @Test
@@ -19,18 +20,36 @@ public class AppTestThreadState {
         Thread thread1 = null;
         Thread thread2 = null;
 
+
+        Runnable runnable = () -> {
+
+            for (int i = 0; i < 10000000; i++) {
+                if (i % 5000 == 0) {
+                    System.out.println("");
+                }
+            }
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        };
+        thread1 = new Thread(runnable);
+        thread2 = new Thread(runnable);
         assertEquals(thread1.getState(), Thread.State.NEW);
         assertEquals(thread2.getState(), Thread.State.NEW);
 
-
         // TODO: fill the gap
+        thread1.start();
         // TODO: fill the gap
-
+        thread2.start();
         assertEquals(thread1.getState(), Thread.State.RUNNABLE);
         assertEquals(thread2.getState(), Thread.State.RUNNABLE);
 
         // Add delay if necessary
         // TODO: fill the gap
+        thread1.sleep(1000);
+        thread2.sleep(1000);
 
         // threads should run task to be put on hold
         assertEquals(thread1.getState(), Thread.State.TIMED_WAITING);

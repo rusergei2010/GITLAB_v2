8d02dbae4f9c8cee80d2ba497e2b5e4b9af38a8c
Nikolai Balabanov <Nikolai_Balabanov@epam.com>
2019-11-26 19:02:25 +0300

    Lecture 5 Fixed

--- Lecture_5_Sync_Wait_Notify_Ctitical_Sections/src/test/java/com/SyncTest.java
+++ Lecture_5_Sync_Wait_Notify_Ctitical_Sections/src/test/java/com/SyncTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
+
 import static org.junit.Assert.assertEquals;
 
 // Mutex, ctitical section in the static method, acquire lock in the same thread (Mutex knows who locked it)
@@ -12,37 +13,38 @@ import static org.junit.Assert.assertEquals;
 // Extrinsic lock is associated with a particular dynamic object (not the Class instance)
 public class SyncTest {
 
-    private static int counter = 0;
+  private static int counter = 0;
 
-    Lock lock = new ReentrantLock();
+  Lock lock = new ReentrantLock();
 
-    public void change() {
+  public void change() {
 
-        lock.lock();
+    if (lock.tryLock()) {
+      try {
         try {
-            try {
-                Thread.sleep(1000);
-            } catch (Exception e){
-                e.printStackTrace();
-            }
-            counter++;
-        } finally {
-            lock.unlock();
+          Thread.sleep(1000);
+        } catch (Exception e) {
+          e.printStackTrace();
         }
+        counter++;
+      } finally {
+        lock.unlock();
+      }
     }
+  }
 
-    @Test
-    public void testSync() {
-        new Thread(() -> {
-            change();
-        }).start();
-        new Thread(() -> {
-            change();
-        }).start();
+  @Test
+  public void testSync() {
+    new Thread(() -> {
+      change();
+    }).start();
+    new Thread(() -> {
+      change();
+    }).start();
 
-        Utils.sleep(2000);
+    Utils.sleep(2000);
 
-        // TODO: fix it with use of 'if(tryLock())' for heavy calculations (~sleep(1000))
-        assertEquals(1, counter);
-    }
+    // TODO: fix it with use of 'if(tryLock())' for heavy calculations (~sleep(1000))
+    assertEquals(1, counter);
+  }
 }

d94fe1ffe1f58d6fb4309640cec2f562146772a4
Natalia Vdovina <nat12-09@mail.ru>
2019-11-29 19:42:39 +0300

    lecture14 hometask

--- Lecture_14_Completable_Future_Concurrency_practice/src/test/java/com/practice/clazz/A3_CompletableFutureExceptionTest.java
+++ Lecture_14_Completable_Future_Concurrency_practice/src/test/java/com/practice/clazz/A3_CompletableFutureExceptionTest.java
@@ -1,17 +1,15 @@
 package com.practice.clazz;
 
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.io.Serializable;
-import java.util.concurrent.Callable;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.FutureTask;
 import java.util.concurrent.ThreadLocalRandom;
 
-@Ignore
+
 public class A3_CompletableFutureExceptionTest {
 
 
@@ -20,10 +18,10 @@ public class A3_CompletableFutureExceptionTest {
         String name = null;
         CompletableFuture<String> handle = CompletableFuture.supplyAsync(() -> {
             if (name == null) {
-                throw new RuntimeException(new Exception("Sergey Exception"));
+                throw new RuntimeException();
             }
             return "Sergey";
-        }).handle((result, ex) -> result != null ? result : "Cause : " + ex.getCause().toString());
+        }).handle((result, ex) -> result != null ? result : "Hey : " + ex.getCause().toString());
 
         System.out.println("Result: " + handle.get());
     }
@@ -32,24 +30,12 @@ public class A3_CompletableFutureExceptionTest {
     @Test
     public void testExecutor() throws ExecutionException, InterruptedException {
         String name = null;
-        final CompletableFuture<String> handle = CompletableFuture.supplyAsync(() -> {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+        CompletableFuture<String> handle = CompletableFuture.supplyAsync(() -> {
+            if (name == null) {
+                throw new RuntimeException();
             }
-
             return "Sergey";
-        }, Executors.newSingleThreadExecutor()).handle((result, ex) -> result != null ? result : "Cause : " + ex.getCause().toString());
-
-        handle.completeExceptionally(new Exception("My exception"));
-        CompletableFuture<String> stringCompletableFuture = CompletableFuture.supplyAsync(
-                new Callable<String>() {
-
-                    @Override public String call() throws Exception {
-                        return null;
-                    }
-                });
+        }, Executors.newSingleThreadExecutor()).handle((result, ex) -> result != null ? result : "Hey : " + ex.getCause().toString());
 
         System.out.println("Result: " + handle.get());
     }

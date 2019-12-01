82c509d304e5161b638ecb57c1eb7c293e5f618e
Maksim Rezchikov <Maksim_Rezchikov@epam.com>
2019-12-01 22:27:31 +0300

    lecture 9 test

--- Lecture_9_Lambda_Java8_Addition/src/test/java/com/data/lambda/part1/exercise/Lambdas03Exercise.java
+++ Lecture_9_Lambda_Java8_Addition/src/test/java/com/data/lambda/part1/exercise/Lambdas03Exercise.java
@@ -18,21 +18,26 @@ public class Lambdas03Exercise {
 
     @Test
     public void generic0() {
-        final GenericProduct<Integer> prod = null; // TODO: Use anonymous class
+        final GenericProduct<Integer> prod = new GenericProduct<Integer>() {
+            @Override
+            public Integer prod(Integer a, int i) {
+                return a*i;
+            }
+        }; // TODO: Use anonymous class
 
         assertEquals(prod.prod(3, 2), Integer.valueOf(6));
     }
 
     @Test
     public void generic1() {
-        final GenericProduct<Integer> prod = null; // TODO: Use statement lambda
+        final GenericProduct<Integer> prod = (a,b) -> {return a*b;}; // TODO: Use statement lambda
 
         assertEquals(prod.prod(3, 2), Integer.valueOf(6));
     }
 
     @Test
     public void generic2() {
-        final GenericProduct<Integer> prod = null; // TODO: Use expression lambda
+        final GenericProduct<Integer> prod = (a,b) -> a*b; // TODO: Use expression lambda
 
         assertEquals(prod.prod(3, 2), Integer.valueOf(6));
     }
@@ -47,7 +52,7 @@ public class Lambdas03Exercise {
 
     @Test
     public void strSum() {
-        final GenericProduct<String> prod = null; // TODO: use convertion to str
+        final GenericProduct<String> prod = (a,b) -> a.concat(a); // TODO: use convertion to str
 
         assertEquals(prod.prod("a", 2), "aa");
     }
